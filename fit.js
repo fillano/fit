8050eb89b98a92b0b6ead9614371399a50878add
Hsu Ping Feng <fillano.feng@gmail.com>
2016-05-31 11:29:14 +0800

    fix scope variables _index, _parent, _global as properties within the object passed to each iteration.

--- fit.js
+++ fit.js
@@ -1,178 +1,215 @@
-if(module) module.exports = fit;
-function fit(str, pre) {
-	if(!String.prototype.trim) {
-		String.prototype.trim = function() {
-			return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+/*
+Copyright (c) 2016 by Hsu Ping Feng <fillano.feng@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+"use strict";
+(function() {
+	var root = this;
+	var old_fit;
+	if(typeof exports !== 'undefined') {
+		if(typeof module !== 'undefined' && !!(module.exports)) {
+			exports = module.exports = fit;
 		}
+		exports.fit = fit;
+	} else {
+		old_fit = root.fit;
+		root.fit = fit;
+		fit.noConflict = function() {
+			root.fit = old_fit;
+			return fit;
+		};
 	}
-	var oplist = [
-		'=',
-		'for',
-		'endfor',
-		'if',
-		'else',
-		'endif'
-	];
-	var exp = {
-		"=": function(_exp) {
-			return function(data) {
-				var e = _exp.substr(1).trim().substr(1).split('.');
-				return e.reduce(function(pre, cur) {
-					return pre[cur];
-				}, data);
-			};
-		},
-		"for": function(_exp, _param) {
-			var e = _exp.substr(3).trim().split('as');
-			var e1 = e[0].trim().substr(1).split('.');
-			var e2 = e[1].trim().substr(1);
-			return function(data) {
-				var s = e1.reduce(function(pre, cur) {
-					return pre[cur];
-				}, data);
-				var ret = '';
-				if(!!s) {
+	function fit(str, pre) {
+		if(!String.prototype.trim) {
+			String.prototype.trim = function() {
+				return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+			}
+		}
+		var oplist = [
+			'=',
+			'for',
+			'endfor',
+			'if',
+			'else',
+			'endif'
+		];
+		var exp = {
+			"=": function(_exp) {
+				return function(data) {
+					var e = _exp.substr(1).trim();
+					e = e.replace(/\$/g, 'data.');
+					return eval(e);
+				};
+			},
+			"for": function(_exp, _param) {
+				var e = _exp.substr(3).trim().split('as');
+				var e1 = e[0].trim().substr(1);
+				var e2 = e[1].trim().substr(1);
+				return function(data, global) {
+					var s = e1.split('.').reduce(function(pre, cur) {
+						return pre[cur];
+					}, data);
+					var s1 = e1.substr(0, e1.lastIndexOf('.')).split('.').reduce(function(pre, cur) {
+						return pre[cur];
+					}, data);
+					var ret = '';
 					s.forEach(function(item, index) {
 						var p = {};
 						p[e2] = item;
-						p[e2]['index'] = index;
+						p[e2]['_index'] = index;
+						p[e2]['_global'] = global;
+						p[e2]['_parent'] = s1;
 						ret += _param.reduce(function(pre, cur) {
 							if(typeof cur === 'function') {
-								return pre + cur(p);
+								if(cur.length == 2) {
+									return pre + cur(p, global);	
+								} else {
+									return pre + cur(p);
+								}
 							} else {
 								return pre + cur;
 							}
 						}, '');
 					});
-				}
-				return ret;
-			};
-		},
-		"if": function(_exp, _param, _else) {
-			var e = _exp.substr(2).trim();
-			return function(data) {
-				e = e.replace(/\$/g, 'data.');
-				var s = eval(e)? _param : _else;
-				if(!s) return '';
-				return s.reduce(function(pre, cur) {
-					if(typeof cur === 'function') {
-						return pre + cur(data);
-					} else {
-						return pre + cur;
-					}
-				}, '');
-			};
-		}
-	};
-	function tokenizer(str) {
-		var l_del = '{{';
-		var r_del = '}}';
-		var part1 = str.split(l_del);
-		return part1.reduce(function(pre, cur) {
-			if(cur.indexOf(r_del) > -1) {
-				var tmp = cur.split(r_del);
-				pre.push('{{OP_' + tmp[0].trimLeft());
-				pre.push(tmp[1]);
-				return pre;
-			} else {
-				pre.push(cur);
-				return pre;		
+					return ret;
+				};
+			},
+			"if": function(_exp, _param, _else) {
+				var e = _exp.substr(2).trim();
+				return function(data) {
+					e = e.replace(/\$/g, 'data.');
+					var s = eval(e)? _param : _else;
+					if(!s) return '';
+					return s.reduce(function(pre, cur) {
+						if(typeof cur === 'function') {
+							return pre + cur(data);
+						} else {
+							return pre + cur;
+						}
+					}, '');
+				};
 			}
-		}, []);
-	}
-	function parser(arr) {
-		var ret = [];
-		var ps = [];
-		arr.forEach(function(cur) {
-			if(cur.indexOf('{{OP_') === 0) {
-				var _exp = cur.substr(5);
-				var _op = _exp.trim().split(/ |\$/)[0];
-				if(oplist.indexOf(_op) > -1) {
-					switch(_op) {
-						case '=':
-							if(ps.length > 0) {
-								if(!!ps[ps.length-1]['else']) {
-									ps[ps.length-1]['else'].push(exp['='](_exp));
+		};
+		function tokenizer(str) {
+			var l_del = '{{';
+			var r_del = '}}';
+			var part1 = str.split(l_del);
+			return part1.reduce(function(pre, cur) {
+				if(cur.indexOf(r_del) > -1) {
+					var tmp = cur.split(r_del);
+					pre.push('{{OP_' + tmp[0].trimLeft());
+					pre.push(tmp[1]);
+					return pre;
+				} else {
+					pre.push(cur);
+					return pre;		
+				}
+			}, []);
+		}
+		function parser(arr) {
+			var ret = [];
+			var ps = [];
+			arr.forEach(function(cur) {
+				if(cur.indexOf('{{OP_') === 0) {
+					var _exp = cur.substr(5);
+					var _op = _exp.trim().split(/ |\$/)[0];
+					if(oplist.indexOf(_op) > -1) {
+						switch(_op) {
+							case '=':
+								if(ps.length > 0) {
+									if(!!ps[ps.length-1]['else']) {
+										ps[ps.length-1]['else'].push(exp['='](_exp));
+									} else {
+										ps[ps.length-1].param.push(exp['='](_exp));
+									}
 								} else {
-									ps[ps.length-1].param.push(exp['='](_exp));
+									ret.push(exp['='](_exp));
 								}
-							} else {
-								ret.push(exp['='](_exp));
-							}
-							break;
-						case 'for':
-							ps.push({exp: _exp, param: []});
-							break;
-						case 'endfor':
-							var e = ps.pop();
-							if(ps.length > 0) {
-								ps[ps.length-1].param.push(exp['for'](e.exp, e.param));
-							} else {
-								ret.push(exp['for'](e.exp, e.param));
-							}
-							break;
-						case 'if':
-							ps.push({exp: _exp, param: []});
-							break;
-						case 'else':
-							ps[ps.length-1]['else'] = [];
-							break;
-						case 'endif':
-							var e = ps.pop();
-							if(ps.length > 0) {
-								if(!!ps[ps.length-1]['else']) {
-									ps[ps.length-1]['else'].push(exp['if'](e.exp, e.param, e['else']));
+								break;
+							case 'for':
+								ps.push({exp: _exp, param: []});
+								break;
+							case 'endfor':
+								var e = ps.pop();
+								if(ps.length > 0) {
+									ps[ps.length-1].param.push(exp['for'](e.exp, e.param));
 								} else {
-									ps[ps.length-1].param.push(exp['if'](e.exp, e.param, e['else']));
+									ret.push(exp['for'](e.exp, e.param));
 								}
+								break;
+							case 'if':
+								ps.push({exp: _exp, param: []});
+								break;
+							case 'else':
+								ps[ps.length-1]['else'] = [];
+								break;
+							case 'endif':
+								var e = ps.pop();
+								if(ps.length > 0) {
+									if(!!ps[ps.length-1]['else']) {
+										ps[ps.length-1]['else'].push(exp['if'](e.exp, e.param, e['else']));
+									} else {
+										ps[ps.length-1].param.push(exp['if'](e.exp, e.param, e['else']));
+									}
+								} else {
+									ret.push(exp['if'](e.exp, e.param, e['else']));
+								}
+								break;
+						}
+					} else {
+						if(ps.length > 0) {
+							if(!!ps[ps.length-1]['else']) {
+								!!ps[ps.length-1]['else'].push('{{unknown('+_op+')}}')
 							} else {
-								ret.push(exp['if'](e.exp, e.param, e['else']));
+								ps[ps.length-1].param.push('{{unknown('+_op+')}}');
 							}
-							break;
+						} else {
+							ret.push('{{unknown('+_op+')}}');
+						}
 					}
 				} else {
 					if(ps.length > 0) {
 						if(!!ps[ps.length-1]['else']) {
-							!!ps[ps.length-1]['else'].push('{{unknown('+_op+')}}')
+							ps[ps.length-1]['else'].push(cur);
 						} else {
-							ps[ps.length-1].param.push('{{unknown('+_op+')}}');
+							ps[ps.length-1].param.push(cur);
 						}
 					} else {
-						ret.push('{{unknown('+_op+')}}');
+						ret.push(cur);
 					}
 				}
-			} else {
-				if(ps.length > 0) {
-					if(!!ps[ps.length-1]['else']) {
-						ps[ps.length-1]['else'].push(cur);
+			});
+			if(ps.length > 0) throw '`for` or `if` statement not terminated.';
+			return ret;
+		}
+		function render(parsed, data) {
+			return parsed.reduce(function(pre, cur) {
+				if(typeof cur === 'function') {
+					if(cur.length === 2) {
+						return pre + cur(data, data);
 					} else {
-						ps[ps.length-1].param.push(cur);
+						return pre + cur(data);
 					}
 				} else {
-					ret.push(cur);
+					return pre + cur;
 				}
-			}
-		});
-		if(ps.length > 0) throw '`for` or `if` statement not terminated.';
-		return ret;
-	}
-	function render(parsed, data) {
-		return parsed.reduce(function(pre, cur) {
-			if(typeof cur === 'function') {
-				return pre + cur(data);
-			} else {
-				return pre + cur;
-			}
-		}, '');
-	}
-	var parsed;
-	if(!!pre) {
-		parsed = parser(tokenizer(str));
-	}
-	return function(data) {
-		if(parsed === undefined) {
+			}, '');
+		}
+		var parsed;
+		if(!!pre) {
 			parsed = parser(tokenizer(str));
 		}
-		return render(parsed, data);
-	};
-}
+		return function(data) {
+			if(parsed === undefined) {
+				parsed = parser(tokenizer(str));
+			}
+			return render(parsed, data);
+		};
+	}
+}).call(this);
--- test/test01.js
+++ test/test01.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert;
 var t = require('../fit');
 describe('ft template engine tests', function() {
-	it('assign', function(done) {
+	it('assign', function() {
 		var expect = '<div>fillano</div>';
 		var s = [
 			'<div>{{=$name}}</div>',
@@ -16,9 +16,14 @@ describe('ft template engine tests', function() {
 		s.forEach(function(s1) {
 			assert.equal(expect, t(s1)({name:'fillano'}));
 		})
-		done();
 	});
-	it('for', function(done) {
+	it('assign with complex expression', function() {
+		var expect = '<div>lastname, firstname</div>';
+		var s = '<div>{{=$name[1]}}, {{=$name[0]}}</div>';
+		var data = {name: ['firstname', 'lastname']};
+		assert.equal(expect, t(s)(data));
+	});
+	it('for', function() {
 		var expect = '<body><div>name1</div><div>name2</div><div>name3</div></body>';
 		var s = [
 			'<body>{{for $names as $name}}<div>{{=$name.name}}</div>{{endfor}}</body>',
@@ -32,25 +37,23 @@ describe('ft template engine tests', function() {
 		s.forEach(function(s1) {
 			assert.equal(expect, t(s1)({names: [{name:'name1'},{name:'name2'},{name:'name3'}]}));
 		});
-		done();
 	});
-	it('if, and index in for', function(done) {
+	it('if, and index in for', function() {
 		var expect = '<body><div style="color:red">name1</div><div style="color:green">name2</div><div style="color:red">name3</div></body>';
 		var s = [
-			'<body>{{for $names as $name}}<div {{if $name.index % 2 === 0}}style="color:red"{{else}}style="color:green"{{endif}}>{{=$name.name}}</div>{{endfor}}</body>',
-			'<body>{{for $names as $name}}<div {{ if $name.index % 2 === 0}}style="color:red"{{ else}}style="color:green"{{endif}}>{{=$name.name}}</div>{{endfor}}</body>',
-			'<body>{{for $names as $name}}<div {{if $name.index % 2 === 0 }}style="color:red"{{else }}style="color:green"{{endif}}>{{=$name.name}}</div>{{endfor}}</body>',
-			'<body>{{for $names as $name}}<div {{ if $name.index % 2 === 0 }}style="color:red"{{ else }}style="color:green"{{ endif }}>{{=$name.name}}</div>{{endfor}}</body>',
-			'<body>{{for $names as $name}}<div {{if  $name.index % 2 === 0}}style="color:red"{{else}}style="color:green"{{endif }}>{{=$name.name}}</div>{{endfor}}</body>',
-			'<body>{{for $names as $name}}<div {{if $name.index%2===0}}style="color:red"{{else}}style="color:green"{{ endif}}>{{=$name.name}}</div>{{endfor}}</body>',
-			'<body>{{for $names as $name}}<div {{if $name.index% 2=== 0 }}style="color:red"{{else}}style="color:green"{{endif}}>{{=$name.name}}</div>{{endfor}}</body>'
+			'<body>{{for $names as $name}}<div {{if $name._index % 2 === 0}}style="color:red"{{else}}style="color:green"{{endif}}>{{=$name.name}}</div>{{endfor}}</body>',
+			'<body>{{for $names as $name}}<div {{ if $name._index % 2 === 0}}style="color:red"{{ else}}style="color:green"{{endif}}>{{=$name.name}}</div>{{endfor}}</body>',
+			'<body>{{for $names as $name}}<div {{if $name._index % 2 === 0 }}style="color:red"{{else }}style="color:green"{{endif}}>{{=$name.name}}</div>{{endfor}}</body>',
+			'<body>{{for $names as $name}}<div {{ if $name._index % 2 === 0 }}style="color:red"{{ else }}style="color:green"{{ endif }}>{{=$name.name}}</div>{{endfor}}</body>',
+			'<body>{{for $names as $name}}<div {{if  $name._index % 2 === 0}}style="color:red"{{else}}style="color:green"{{endif }}>{{=$name.name}}</div>{{endfor}}</body>',
+			'<body>{{for $names as $name}}<div {{if $name._index%2===0}}style="color:red"{{else}}style="color:green"{{ endif}}>{{=$name.name}}</div>{{endfor}}</body>',
+			'<body>{{for $names as $name}}<div {{if $name._index% 2=== 0 }}style="color:red"{{else}}style="color:green"{{endif}}>{{=$name.name}}</div>{{endfor}}</body>'
 		];
 		s.forEach(function(s1) {
 			assert.equal(expect, t(s1)({names: [{name:'name1'},{name:'name2'},{name:'name3'}]}));
 		});
-		done();
 	});
-	it('for in for', function(done) {
+	it('for in for', function() {
 		var expect = '<body><div><h3>name1</h3><div>child1-1</div><div>child1-2</div><div>child1-3</div></div><div><h3>name2</h3><div>child2-1</div><div>child2-2</div><div>child2-3</div></div><div><h3>name3</h3><div>child3-1</div><div>child3-2</div><div>child3-3</div></div></body>';
 		var s = '<body>{{for $people as $person}}<div><h3>{{=$person.name}}</h3>{{for $person.children as $child}}<div>{{=$child.name}}</div>{{endfor}}</div>{{endfor}}</body>';
 		var data = {
@@ -82,28 +85,72 @@ describe('ft template engine tests', function() {
 			]
 		};
 		assert.equal(expect, t(s)(data));
-		done();
 	});
-	it('if expression is false without else tag', function(done) {
+	it('if expression is false without else tag', function() {
 		var expect = '<div></div>';
 		var s = '<div>{{if false}}{{=$name}}{{endif}}</div>';
 		var data = {name: 'okbyme'};
 		assert.equal(expect, t(s)(data));
-		done();
 	});
-	it('if expression is true with else tag', function(done) {
+	it('if expression is true with else tag', function() {
 		var expect = '<div></div>';
 		var s = '<div>{{if true}}{{else}}{{=$name}}{{endif}}</div>';
 		var data = {name: 'okbyme'};
 		assert.equal(expect, t(s)(data));
-		done();
 	});
-	it('with or without pre compiled template should have the same result', function(done) {
+	it('with or without pre compiled template should have the same result', function() {
 		var expect = '<div>okbyme</div>';
 		var s = '<div>{{=$name}}</div>';
 		var data = {name: 'okbyme'};
 		assert.equal(expect, t(s)(data));//template compile in runtime
 		assert.equal(expect, t(s, true)(data));//precompile template
-		done();
+	});
+	it('test for scope variable _global in single for loop', function() {
+		var expect = '<div>(A)</div><div>(B)</div><div>(C)</div><div>(D)</div>';
+		var s = '{{for $qa as $q}}<div>({{=$q._global.listitem[$q._index]}}){{=$q.content}}</div>{{endfor}}'
+		var data = {
+			listitem: ['A','B','C','D'],
+			qa: [
+				{content: ''},
+				{content: ''},
+				{content: ''},
+				{content: ''}
+			]
+		}
+		assert.equal(expect, t(s)(data));
+	});
+	it('test for scope variable _global in double for loop', function() {
+		var expect = '<div>question1<ol><li><input type="radio" name="q1" value="0" /> (A) </li><li><input type="radio" name="q1" value="1" /> (B) </li><li><input type="radio" name="q1" value="2" /> (C) </li><li><input type="radio" name="q1" value="3" /> (D) </li></ol></div><div>question2<ol><li><input type="radio" name="q2" value="0" /> (A) </li><li><input type="radio" name="q2" value="1" /> (B) </li><li><input type="radio" name="q2" value="2" /> (C) </li><li><input type="radio" name="q2" value="3" /> (D) </li></ol></div><div>question3<ol><li><input type="radio" name="q3" value="0" /> (A) </li><li><input type="radio" name="q3" value="1" /> (B) </li><li><input type="radio" name="q3" value="2" /> (C) </li><li><input type="radio" name="q3" value="3" /> (D) </li></ol></div>';
+		var s = '{{for $q as $qs}}<div>{{=$qs.desc}}<ol>{{for $qs.a as $ad}}<li><input type="radio" name="{{=$ad[\'_parent\'].id}}" value="{{=$ad[\'_index\']}}" /> ({{=$ad[\'_global\'].listitem[$ad[\'_index\']]}}) {{=$ad.desc}}</li>{{endfor}}</ol></div>{{endfor}}';
+		var data = {
+			listitem: ['A','B','C','D'],
+			q: [
+				{
+					desc: 'question1',
+					id: 'q1',
+					a: [{desc:''},{desc:''},{desc:''},{desc:''}]
+				},
+				{
+					desc: 'question2',
+					id: 'q2',
+					a: [{desc:''},{desc:''},{desc:''},{desc:''}]
+				},
+				{
+					desc: 'question3',
+					id: 'q3',
+					a: [{desc:''},{desc:''},{desc:''},{desc:''}]
+				}
+			]
+		};
+		assert.equal(expect, t(s)(data));
+	});
+	it('test for reuse template with different data', function() {
+		var expect1 = '<div>name1</div>';
+		var expect2 = '<div>name2</div>';
+		var s = '<div>{{=$name}}</div>';
+		var data1 = {name:'name1'};
+		var data2 = {name:'name2'};
+		assert.equal(expect1, t(s)(data1));
+		assert.equal(expect2, t(s)(data2));
 	});
 });
